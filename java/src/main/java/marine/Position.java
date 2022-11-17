e2dcf2edc5d17952300ad79a0eebecd9146fc254
Yoan Thirion <yoan.thirion@gmail.com>
2022-11-17 17:36:28 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 7
        test: 0
    test-stats:
        run: 1
        passed: 1
        failed: 0
        skipped: 0
        error: 0
        duration: 78ms

--- java/src/main/java/marine/Position.java
+++ java/src/main/java/marine/Position.java
@@ -3,10 +3,7 @@ package marine;
 import lombok.AllArgsConstructor;
 import lombok.With;
 
-@AllArgsConstructor
-@With
+@AllArgsConstructor @With
 public class Position {
-    public final int horizontal;
-    public final int profondeur;
-
+    public int horizontal, profondeur;
 }
